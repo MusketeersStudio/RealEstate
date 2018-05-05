1799b261da69483990521c2c1028540a5eb91353
drh <jmnabangi@gmail.com>
2018-05-05 15:53:37 +0300

    Added Placeholder Blade Files; Added Basic Routes; Added Tenant Controller;

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,31 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/properties', function () {
+    return view('properties');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/inquiry', function () {
+    return view('inquiry');
+});
+Route::get('/', function () {
+    return view('index');
+});
+
+
+Route::get('/property-manager', 'PropertyManagerController@index');
+
+Route::get('/service-provider', 'ServiceProviderController@index');
+
+Route::get('/tenant', 'TenantController@index');
+
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        // Matches The "/admin/users" URL
+    });
 });
